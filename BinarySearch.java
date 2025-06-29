51867d387739d0646244bf1373ed77774fb04739
ArchanaDinaRoy <archanaroy2003@gmail.com>
2025-06-29 21:41:38 +0530

    Binary Search take input
    Binary Search take input

---
+++ BinarySearch.java
@@ -0,0 +1,49 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class BinarySearch {
+
+
+    public static int bSearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        System.out.println("Enter sorted array elements:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target to search: ");
+        int target = sc.nextInt();
+
+        int result = bSearch(arr, target);
+        if (result != -1) {
+            System.out.println("Target found at index: " + result);
+        } else {
+            System.out.println("Target not found.");
+        }
+    }
+}
